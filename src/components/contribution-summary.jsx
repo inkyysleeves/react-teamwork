93c5c89d9beae3b67a4b27834a53ebb0b22eb943
Geevan Patel <geevan.peter.patel@gmail.com>
2019-04-12 00:00:42 +0100

    first commit

--- src/components/contribution-summary.jsx
+++ src/components/contribution-summary.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
 
-const ContributionSummary = (props) => <div>Contribution Summary for: {props.username}</div>;
+class Contribution extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contributions: null,
+      pushEvents: [],
+    };
+  }
 
-export default ContributionSummary;
+  componentDidMount() {
+    fetch(`https://mcr-codes-cohorts.herokuapp.com/users/${this.props.username}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ contributions: data.contributions });
+        return data;
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({ pushEvents: data.events });
+      });
+  }
+
+
+  render() {
+    const { contributions } = this.state;
+    console.log(this.state.pushEvents);
+    return this.state.contributions ? (
+      <div>
+        <p>
+           Contribution Summary for: {this.props.username}
+        </p>
+        <p>
+           total {contributions} contributions.
+        </p>
+        <p>
+          {contributions} contributions in the last month.
+        </p>
+        <p>
+          {contributions} contributions in the last week.
+        </p>
+      </div>
+    ) : (
+      <div>
+          LOADING
+      </div>
+    );
+  }
+}
+
+export default Contribution;
