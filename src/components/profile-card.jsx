5799d423d2594c36093bf93069ae5f1025b8dfe2
Russell <russelldoe@hotmail.co.uk>
2019-04-11 21:20:21 +0100

    profile card complete

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -1,5 +1,42 @@
 import React from 'react';
 
-const ProfileCard = (props) => <div>Profile Card for: {props.username}</div>;
+const url = 'https://mcr-codes-cohorts.herokuapp.com/users/';
+
+class ProfileCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${url}${this.props.username}`)
+      .then(response => response.json())
+      .then(data => this.setState({ profile: data.profile }));
+  }
+   
+  
+  render() {
+    console.log(this.state);
+  if (this.state.profile !== null) {
+      return ( 
+    <div>
+      <img src={this.state.profile.avatar} />
+      <br />
+      <h2>{this.state.profile.login}</h2>
+      <br />
+      <h3>{this.state.profile.location}</h3>
+      <br />
+      <h3>Repos: {this.state.profile.repos}</h3>
+      <br />
+      <h3>Following: {this.state.profile.following}</h3>
+    </div>
+    );
+  } else {
+      return (<div>pending</div>)
+  }
+ } 
+}
 
 export default ProfileCard;
