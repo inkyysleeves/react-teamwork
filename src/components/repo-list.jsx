933baf5b2d2daf5444167019e3a4b75797f473d2
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-04-11 21:06:06 +0100

    Completed basic functionality - no styling

--- src/components/repo-list.jsx
+++ src/components/repo-list.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import RepoCard from './repo-card';
 
-const RepoList = (props) => (
-  <div>
-    <p>Repo List for: {props.username}</p>
-    <ul>
-      <RepoCard username={props.username} />
-      <RepoCard username={props.username} />
-      <RepoCard username={props.username} />
-    </ul>
-  </div>
-);
+const name = 'jackpearsall';
+
+class RepoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
+
+  componentDidMount() {
+    const DATA_URL = `https://mcr-codes-cohorts.herokuapp.com/users/${name}`;
+    fetch(DATA_URL)
+      .then(res => res.json())
+      .then(data => {
+        return this.setState({ data });
+      });
+  }
+
+  render() {
+    const { repos } = this.state.data;
+    return (
+      <div>{ !repos ? <div>loading</div> : repos.map((repo, index) => {
+        return (
+          <RepoCard
+            key={index} 
+            name={repo.name}
+            description={repo.description}
+            language={repo.language}
+            stars={repo.watchers}
+            forks={repo.forks}
+          />
+        );
+      })}
+      </div>
+    );
+  }
+}
 
 export default RepoList;
