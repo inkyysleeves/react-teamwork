ee57a8af06aa6191c5f7a8ad4569b727f7e58b6b
tom bilbe <tbilbe@gmail.com>
2019-04-12 13:11:02 +0100

    toms button - commented out componentDidMount also

--- src/components/repo-list.jsx
+++ src/components/repo-list.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import RepoCard from './repo-card';
 
-const name = 'jackpearsall';
+const name = 'tbilbe';
 
 class RepoList extends React.Component {
   constructor(props) {
@@ -9,33 +9,51 @@ class RepoList extends React.Component {
     this.state = {
       data: {},
     };
-  }
 
-  componentDidMount() {
+  }
+  goGetFetch() {
     const DATA_URL = `https://mcr-codes-cohorts.herokuapp.com/users/${name}`;
     fetch(DATA_URL)
       .then(res => res.json())
-      .then(data => {
-        return this.setState({ data });
-      });
+      .then(data => { return this.setState({ data });
+    });
   }
+
+  handleButtonClick = (props) => {
+    const DATA_URL = `https://mcr-codes-cohorts.herokuapp.com/users/${name}`;
+    console.log(this)
+    this.goGetFetch() 
+  }
+
+  // componentDidMount() {
+  //   const DATA_URL = `https://mcr-codes-cohorts.herokuapp.com/users/${name}`;
+  //   fetch(DATA_URL)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       return this.setState({ data });
+  //     });
+  // }
 
   render() {
     const { repos } = this.state.data;
     return (
-      <div>{ !repos ? <div>loading</div> : repos.map((repo, index) => {
-        return (
-          <RepoCard
-            key={index} 
-            name={repo.name}
-            description={repo.description}
-            language={repo.language}
-            stars={repo.watchers}
-            forks={repo.forks}
-          />
-        );
-      })}
+      <div>
+        <button onClick={this.handleButtonClick}>Click meee!</button>
+        <div>{ !repos ? <div>loading</div> : repos.map((repo, index) => {
+          return (
+            <RepoCard
+              key={index} 
+              name={repo.name}
+              description={repo.description}
+              language={repo.language}
+              stars={repo.watchers}
+              forks={repo.forks}
+            />
+          );
+        })}
+        </div>
       </div>
+      
     );
   }
 }
