2f1c10907ebc51ece9e3d89507148d672a304927
Ersel Aker <erselaker@gmail.com>
2019-04-16 20:38:48 +0100

    refactored

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -1,44 +1,26 @@
 import React from 'react';
 
-const url = 'https://mcr-codes-cohorts.herokuapp.com/users/';
+const ProfileCard = (props) => {
+  const {
+    avatar, login, location, repos, following,
+  } = props.profile;
 
-class ProfileCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile: {},
-    };
-  }
+  return (
+    <div>
+      <img src={avatar} />
+      <br />
+      <h2>{login}</h2>
+      <br />
+      <h3>{location}</h3>
+      <br />
+      <h3>Repos: {repos}</h3>
+      <br />
+      <h3>Following: {following}</h3>
+    </div>
+  );
 
-  componentDidMount() {
-    fetch(`${url}${this.props.username}`)
-      .then(response => response.json())
-      .then(data => this.setState({ profile: data.profile }));
-  }
+  return <div>pending</div>;
+};
 
-  render() {
-    const {
-      avatar, login, location, repos, following,
-    } = this.state.profile;
-
-    if (login !== null) {
-      return (
-        <div>
-          <img src={avatar} />
-          <br />
-          <h2>{login}</h2>
-          <br />
-          <h3>{location}</h3>
-          <br />
-          <h3>Repos: {repos}</h3>
-          <br />
-          <h3>Following: {following}</h3>
-        </div>
-      );
-    }
-
-    return <div>pending</div>;
-  }
-}
 
 export default ProfileCard;
--- src/components/repo-list.jsx
+++ src/components/repo-list.jsx
@@ -1,47 +1,25 @@
 import React from 'react';
 import RepoCard from './repo-card';
 
-const URL = 'https://mcr-codes-cohorts.herokuapp.com/users/';
+const RepoList = (props) => {
+  const { repos } = props;
 
-class RepoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      repos: null,
-    };
-  }
+  return (
+    <div>{ repos.map((repo) => {
+      return (
+        <RepoCard
+          key={repo.name}
+          name={repo.name}
+          description={repo.description}
+          language={repo.language}
+          stars={repo.watchers}
+          forks={repo.forks}
+        />
+      );
+    })}
+    </div>
+  );
+};
 
-  componentDidMount() {
-    fetch(`${URL}${this.props.username}`)
-      .then(res => res.json())
-      .then(data => {
-        return this.setState({ repos: data.repos });
-      });
-  }
-
-  render() {
-    const { repos } = this.state;
-
-    if (!repos) {
-      return <div>loading</div>;
-    }
-
-    return (
-      <div>{ repos.map((repo) => {
-        return (
-          <RepoCard
-            key={repo.name}
-            name={repo.name}
-            description={repo.description}
-            language={repo.language}
-            stars={repo.watchers}
-            forks={repo.forks}
-          />
-        );
-      })}
-      </div>
-    );
-  }
-}
 
 export default RepoList;
