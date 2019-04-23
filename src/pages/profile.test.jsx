f9ae61e3c7e09bb57231b0c3e819f153732dc9d0
Ersel Aker <erselaker@gmail.com>
2019-04-23 19:55:35 +0100

    add more snapshots

--- src/pages/profile.test.jsx
+++ src/pages/profile.test.jsx
@@ -21,7 +21,7 @@ const MOCK_GITHUB_API = Promise.resolve(MOCK_API_DATA);
 fetchGitHubProfile.mockImplementation(() => MOCK_GITHUB_API);
 
 describe('Profile page', () => {
-  it('should render', (done) => {
+  it('should render ContributionSummary', (done) => {
     const dummyRoute = {
       params: {
         username: 'ersel',
@@ -30,7 +30,35 @@ describe('Profile page', () => {
     const myProfile = shallow(<Profile match={dummyRoute} />);
     // wait for api to resolve then call jest done()
     MOCK_GITHUB_API.then(() => {
-      expect(myProfile).toMatchSnapshot();
+      expect(myProfile.find('ContributionSummary').props()).toMatchSnapshot();
+      done();
+    });
+  });
+
+  it('should render RepoList', (done) => {
+    const dummyRoute = {
+      params: {
+        username: 'ersel',
+      },
+    };
+    const myProfile = shallow(<Profile match={dummyRoute} />);
+    // wait for api to resolve then call jest done()
+    MOCK_GITHUB_API.then(() => {
+      expect(myProfile.find('RepoList').props()).toMatchSnapshot();
+      done();
+    });
+  });
+
+  it('should render ProfileCard', (done) => {
+    const dummyRoute = {
+      params: {
+        username: 'ersel',
+      },
+    };
+    const myProfile = shallow(<Profile match={dummyRoute} />);
+    // wait for api to resolve then call jest done()
+    MOCK_GITHUB_API.then(() => {
+      expect(myProfile.find('ProfileCard')).toMatchSnapshot();
       done();
     });
   });
